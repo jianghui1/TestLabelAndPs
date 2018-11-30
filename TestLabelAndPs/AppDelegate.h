d6fd68503a0bf07a5fbc60b00cbfdeb7eb9409c7
jiangzenghui005@163.com <jiangzenghui005@163.com>
2018-11-30 17:00:54 +0800

    Initial Commit

---
+++ TestLabelAndPs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestLabelAndPs
+//
+//  Created by ys on 2018/11/30.
+//  Copyright © 2018 mg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestLabelAndPs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestLabelAndPs
+//
+//  Created by ys on 2018/11/30.
+//  Copyright © 2018 mg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
